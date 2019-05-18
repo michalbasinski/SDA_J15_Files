5a191fc8c083573e4c2c50523bb71c0ea739517e
Michał Basiński <michal.basinski88@gmail.com>
2019-05-18 10:35:05 +0200

    org.json

---
+++ src/main/java/pl/sda/json/JsonParser.java
@@ -0,0 +1,39 @@
+package pl.sda.json;
+
+import org.json.JSONObject;
+import pl.sda.model.Address;
+import pl.sda.model.Person;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonParser {
+    public Person readJson(String filePath) throws IOException {
+
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        String content = new String(bytes, "UTF-8");
+
+        JSONObject json = new JSONObject(content);
+
+        Person person = new Person();
+        String name = json.getString("name");
+        String lastName = json.getString("lastName");
+
+        JSONObject address =
+                new JSONObject(json.get("address").toString());
+        String city = address.getString("city");
+        String street = address.getString("street");
+        String postalCode = address.getString("postalCode");
+        Address addressObject = new Address();
+        addressObject.setCity(city);
+        addressObject.setStreet(street);
+        addressObject.setPostalCode(postalCode);
+
+        person.setName(name);
+        person.setLastName(lastName);
+        person.setAddress(addressObject);
+
+        return person;
+    }
+}
